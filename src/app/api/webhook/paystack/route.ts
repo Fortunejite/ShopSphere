687f51ec2d63840984ad98ad457cbf5856acfcae
Fortune <fortunejite1234@gmail.com>
2026-04-28 10:28:30 +0100

    fix: update Paystack webhook handler to use parsed JSON body

--- src/app/api/webhook/paystack/route.ts
+++ src/app/api/webhook/paystack/route.ts
@@ -3,8 +3,9 @@ import { paystackWebhookHandler, verifyPaystackSignature } from '@/services/pays
 import { NextResponse } from 'next/server';
 
 export const POST = errorHandler(async (req) => {
+  const body = await req.json()
   const isSignatureValid = verifyPaystackSignature(
-    await req.text(),
+    await body.toString(),
     req.headers.get('x-paystack-signature'),
   );
   if (!isSignatureValid) {
@@ -14,7 +15,7 @@ export const POST = errorHandler(async (req) => {
     );
   }
 
-  await paystackWebhookHandler(await req.json())
+  await paystackWebhookHandler(body);
 
   return NextResponse.json({ received: true });
 });
